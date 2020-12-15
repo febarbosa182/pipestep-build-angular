e1f25f5e4f2b463d0a44e0ab0b8159ab41e93380
felipeasynvest <felipe.barbosa@easynvest.com.br>
2020-12-15 00:06:46 -0300

    init: build angular app module

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build
+
+Pipeline step responsible for build Angular projects with npm
---
+++ src/br/com/easynvest/build/Build.groovy
@@ -0,0 +1,24 @@
+package br.com.easynvest.build
+
+class Build{
+    def call (jenkins, jenkinsParams) {
+        jenkins.podTemplate(
+            containers: [
+                jenkins.containerTemplate(name: 'node', image: jenkins.env.CI_IMAGE, ttyEnabled: true, command: 'cat')
+            ],
+            yamlMergeStrategy: jenkins.merge(),
+            workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
+                claimName: "pvc-${jenkins.env.JENKINS_AGENT_NAME}",
+                readOnly: false
+            )
+        )
+
+        {
+            jenkins.node(jenkins.POD_LABEL){
+                jenkins.container('node'){
+                    jenkins.sh label: "Build application", script: "npm run build"
+                }
+            }
+        }
+    }
+}
